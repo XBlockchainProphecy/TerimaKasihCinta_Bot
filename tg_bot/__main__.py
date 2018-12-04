a92a4803bf5b86fcf5f87a51faf649d23bc4a137
XBlockchainProphecy <45601601+XBlockchainProphecy@users.noreply.github.com>
2018-12-04 22:27:32 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,15 +19,11 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - @XBlockchainProphecy.
 
 I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
-python-telegram-bot library, and am fully [opensource](https://github.com/paulsonoflars/tgbot) - you can find what makes me tick \
-[here](github.com/aboyhasnooname/tgbot)!
-
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+python-telegram-bot library, and am fully - you can find what makes me tick \
+[here](https://t.me/XBlockchainProphecy)! 
 
 You can find the list of available commands with /help.
 
@@ -53,10 +49,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/XBlockchainProphecy) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+(see his bio!). He's just a Boss, so every little helps!
+There are two ways of paying him; [BITCOIN](t.me/XBlockchainProphecy), or [Ethereum](t.me/XBlockchainProphecy)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
